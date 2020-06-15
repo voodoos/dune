b16af598f291352a8968b6e30040a984947a6ab3
Ulysse Gérard <thevoodoos@gmail.com>
2020-06-15 16:32:28 +0200

    Update test suite
    Signed-off-by: Ulysse Gérard <thevoodoos@gmail.com>

--- test/blackbox-tests/test-cases/merlin/merlin-tests.t/sanitize-dot-merlin/sanitize_dot_merlin.ml
+++ test/blackbox-tests/test-cases/merlin/merlin-tests.t/sanitize-dot-merlin/sanitize_dot_merlin.ml
@@ -1,16 +1,21 @@
 open Printf
 
 let process_line =
-  let path_re = Str.regexp {|^\([SB]\) /.+/lib/\(.+\)$|} in
-  let ppx_re = Str.regexp {|^FLG -ppx '/.+/\.ppx/\(.+\)$|} in
-  let special_pp_re =
-    Str.regexp {|^FLG -pp '/.+/_build/install/default/bin/\(.+\)$|}
-  in
+  let path_re = Str.regexp {|\((1:[SB]\)[0-9]+:/[^)]+/lib\(/?[^)]*)\)|} in
+  let ppx_re = Str.regexp {|\((3:FLG\)[0-9]+:-ppx '/[^)]+/\.ppx/\([^)]+)\)|} in
+  (* let special_pp_re =
+    Str.regexp {|\((3:FLG\)[0-9]+:-pp '/[^)]+/_build/install/default/bin/\([^)]+)\)|}
+  in *)
+  let endline_re = Str.regexp {|)(|} in
+  let sizes_re = Str.regexp {|[0-9]+:|} in
   fun line ->
     line
-    |> Str.replace_first path_re {|\1 $LIB_PREFIX/lib/\2|}
-    |> Str.global_replace ppx_re {|FLG -ppx '$PPX/\1|}
-    |> Str.global_replace special_pp_re {|FLG -pp '$BIN/\1|}
+    (* 
+    |> Str.global_replace special_pp_re "\\1 -pp '$BIN/\\2" *)
+    |> Str.global_replace ppx_re "\\1?:-ppx '$PPX/\\2"
+    |> Str.global_replace path_re "\\1?:$LIB_PREFIX/lib\\2"
+    |> Str.global_replace endline_re ")\n("
+    |> Str.global_replace sizes_re "?:"
 
 let () =
   let files = Sys.argv |> Array.to_list |> List.tl |> List.sort compare in
