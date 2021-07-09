0272ea09318d84b3de2a71a5b829a95a080a348e
Ulysse Gérard <thevoodoos@gmail.com>
2021-07-09 14:53:42 +0200

    [wip] foreign_archive for both modes
    Signed-off-by: Ulysse Gérard <thevoodoos@gmail.com>

--- src/dune_rules/dune_file.ml
+++ src/dune_rules/dune_file.ml
@@ -153,7 +153,7 @@ module Buildable = struct
     ; modules : Ordered_set_lang.t
     ; modules_without_implementation : Ordered_set_lang.t
     ; libraries : Lib_dep.t list
-    ; foreign_archives : (Loc.t * Foreign.Archive.t) list
+    ; foreign_archives : (Loc.t * Foreign.Archive.t) list Mode.Dict.t
     ; foreign_stubs : Foreign.Stubs.t list
     ; preprocess : Preprocess.With_instrumentation.t Preprocess.Per_module.t
     ; preprocessor_deps : Dep_conf.t list
@@ -306,6 +306,8 @@ module Buildable = struct
            the "lib" prefix, however, since standard linkers require it). *)
         | Some name -> (loc, Foreign.Archive.stubs name) :: foreign_archives
     in
+    (* TODO ulysse check *)
+    let foreign_archives = Mode.Dict.make_both foreign_archives in
     { loc
     ; preprocess
     ; preprocessor_deps
@@ -322,7 +324,8 @@ module Buildable = struct
     }
 
   let has_foreign t =
-    List.is_non_empty t.foreign_stubs || List.is_non_empty t.foreign_archives
+    List.is_non_empty t.foreign_stubs
+    || Mode.Dict.exists t.foreign_archives ~f:List.is_non_empty
 end
 
 module Public_lib = struct
@@ -752,19 +755,19 @@ module Library = struct
 
   let has_foreign t = Buildable.has_foreign t.buildable
 
-  let foreign_archives t =
+  let foreign_archives t mode =
     (if List.is_empty t.buildable.foreign_stubs then
       []
     else
       [ Foreign.Archive.stubs (Lib_name.Local.to_string (snd t.name)) ])
-    @ List.map ~f:snd t.buildable.foreign_archives
+    @ List.map ~f:snd (Mode.Dict.get t.buildable.foreign_archives mode)
 
-  let foreign_lib_files t ~dir ~ext_lib =
-    List.map (foreign_archives t) ~f:(fun archive ->
-        Foreign.Archive.lib_file ~archive ~dir ~ext_lib)
+  let foreign_lib_files t ~mode ~dir ~ext_lib =
+    List.map (foreign_archives t mode) ~f:(fun archive ->
+        Foreign.Archive.lib_file ~archive mode ~dir ~ext_lib)
 
   let foreign_dll_files t ~dir ~ext_dll =
-    List.map (foreign_archives t) ~f:(fun archive ->
+    List.map (foreign_archives t Byte) ~f:(fun archive ->
         Foreign.Archive.dll_file ~archive ~dir ~ext_dll)
 
   let archive_basename t ~ext = Lib_name.Local.to_string (snd t.name) ^ ext
@@ -835,7 +838,9 @@ module Library = struct
       | Public p -> Public (conf.project, p.package)
     in
     let virtual_library = is_virtual conf in
-    let foreign_archives = foreign_lib_files conf ~dir ~ext_lib in
+    let foreign_archives =
+      Mode.Dict.of_func (foreign_lib_files conf ~dir ~ext_lib)
+    in
     let native_archives =
       let archive = archive ext_lib in
       if virtual_library || not modes.native then
