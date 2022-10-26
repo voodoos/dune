e9ee8ab3cd18990f059f312b2aba40107c244ff0
Ulysse Gérard <thevoodoos@gmail.com>
2022-10-26 15:29:04 +0200

    Revert "Communicate source root to Merlin"
    This reverts commit c245680e9d23139cd29a0ba3f00249f2216b4777.

--- src/dune_rules/merlin.ml
+++ src/dune_rules/merlin.ml
@@ -62,9 +62,6 @@ module Processed = struct
       make_directive tag (Sexp.Atom (serialize_path path))
     in
     let build_dir = [ make_directive_of_path "BUILD_DIR" build_dir ] in
-    let source_root =
-      [ make_directive_of_path "PROJECT_ROOT" @@ Path.source Path.Source.root ]
-    in
     let stdlib_dir = [ make_directive_of_path "STDLIB" stdlib_dir ] in
     let exclude_query_dir = [ Sexp.List [ Atom "EXCLUDE_QUERY_DIR" ] ] in
     let obj_dirs =
@@ -94,7 +91,6 @@ module Processed = struct
     Sexp.List
       (List.concat
          [ build_dir
-         ; source_root
          ; stdlib_dir
          ; exclude_query_dir
          ; obj_dirs
