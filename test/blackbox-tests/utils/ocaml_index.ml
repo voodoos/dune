c74ec7b056f04c929f8f5ee48c43ab0613e1cd09
Ulysse Gérard <thevoodoos@gmail.com>
2024-05-07 12:24:52 +0200

    Format the mock indexer

--- test/blackbox-tests/utils/ocaml_index.ml
+++ test/blackbox-tests/utils/ocaml_index.ml
@@ -5,6 +5,7 @@ open Cmdliner
 let touch file =
   let chan = open_out file in
   close_out chan
+;;
 
 module Common = struct
   let set_log_level _ _ = ()
@@ -12,19 +13,19 @@ module Common = struct
   let verbose =
     let doc = "increase log verbosity" in
     Arg.(value & flag & info [ "v"; "verbose" ] ~doc)
+  ;;
 
   let debug =
     let doc = "set maximum log verbosity" in
     Arg.(value & flag & info [ "debug" ] ~doc)
+  ;;
 
-  let with_log = Term.(const set_log_level $ debug $ verbose )
+  let with_log = Term.(const set_log_level $ debug $ verbose)
 
   let output_file =
     let doc = "name of the generated index" in
-    Arg.(
-      value
-      & opt string "project.ocaml-index"
-      & info [ "o"; "output-file" ] ~doc)
+    Arg.(value & opt string "project.ocaml-index" & info [ "o"; "output-file" ] ~doc)
+  ;;
 end
 
 module Aggregate = struct
@@ -33,32 +34,41 @@ module Aggregate = struct
   let root =
     let doc = "if provided all locations will be appended to that path" in
     Arg.(value & opt (some string) None & info [ "root" ] ~doc)
+  ;;
 
   let files =
     let doc = "the files to index" in
     Arg.(value & pos_all string [] & info [] ~doc)
+  ;;
 
   let build_path =
     let doc = "an extra directory to add to the load path" in
     Arg.(value & opt_all string [] & info [ "I" ] ~doc)
+  ;;
 
   let store_shapes =
-    let doc =
-      "aggregate input-indexes shapes and store them in the new index"
-    in
+    let doc = "aggregate input-indexes shapes and store them in the new index" in
     Arg.(value & flag & info [ "store-shapes" ] ~doc)
+  ;;
 
   let term =
     Term.(
-      const from_files $ store_shapes $ root $ Common.output_file $ build_path
-      $ files $ Common.with_log)
+      const from_files
+      $ store_shapes
+      $ root
+      $ Common.output_file
+      $ build_path
+      $ files
+      $ Common.with_log)
+  ;;
 
   let cmd =
     let info =
       let doc = "builds the index for a single $(i, .cmt) file" in
       Cmd.info "aggregate" ~doc
     in
-    Cmd.v info ( term)
+    Cmd.v info term
+  ;;
 end
 
 module Dump = struct
@@ -67,6 +77,7 @@ module Dump = struct
   let file =
     let doc = "the file to dump" in
     Arg.(required & pos 0 (some string) None & info [] ~doc)
+  ;;
 
   let term = Term.(const dump $ file $ Common.with_log)
 
@@ -76,6 +87,7 @@ module Dump = struct
       Cmd.info "dump" ~doc
     in
     Cmd.v info term
+  ;;
 end
 
 let subcommands =
@@ -84,5 +96,6 @@ let subcommands =
     Cmd.info "ocaml-index" ~doc
   in
   Cmd.group info ~default:Aggregate.term [ Aggregate.cmd; Dump.cmd ]
+;;
 
 let () = exit (Cmd.eval subcommands)
